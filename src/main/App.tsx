277677984044e56100c6d6427b369cb6266b0339
erickLuizA <erickluiz69@gmail.com>
2020-12-10 11:11:02 -0300

    timeout to hide splash when all is loaded

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -8,7 +8,9 @@ import Navigation from './navigation'
 
 export default function App() {
   useEffect(() => {
-    SplashScreen.hide()
+    setTimeout(() => {
+      SplashScreen.hide()
+    }, 1000)
   }, [])
 
   return (
